43742dead08b28f2689f121755cf7c0499a31f01
imkadam <imkadam@hotmail.my>
2025-06-07 16:57:54 +0300

    new changes

--- init.lua
+++ init.lua
@@ -1,3 +1,50 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.wo.relativenumber = true
+vim.g.lazyvim_python_lsp = "basedpyright"
+
+require("lspconfig").basedpyright.setup({
+  settings = {
+    basedpyright = {
+      typeCheckingMode = "off",
+      useLibraryCodeForTypes = true,
+      diagnosticMode = "workspace", -- "openFilesOnly" or "workspace"
+      reportMissingImports = true, -- Set to false to disable missing import warnings
+      reportMissingTypeStubs = false, -- Disable missing type stub warnings
+      reportOptionalMemberAccess = false, -- Disable optional member access warnings
+      reportOptionalSubscript = false, -- Disable optional subscript warnings
+      reportOptionalCall = false, -- Disable optional call warnings
+      reportPrivateImportUsage = false, -- Disable warnings about private imports
+      autoSearchPaths = true,
+
+      venvPath = ".",
+      venv = "env",
+    },
+  },
+})
+
+require("lspconfig").pylsp.setup({
+  settings = {
+    pylsp = {
+      plugins = {
+        jedi_completion = { enabled = true },
+        jedi_hover = { enabled = false },
+        jedi_signature_help = { enabled = false },
+        pyflakes = { enabled = true },
+        pycodestyle = { enabled = false },
+        mccabe = { enabled = true },
+        pylint = { enabled = false },
+        flake8 = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        black = { enabled = false },
+        -- autopape8 = { enabled = false },
+        -- rope_autoimport = { enabled = false }, -- if installed
+        -- You can add more plugins here if you use others
+      },
+    },
+  },
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingprovider = false
+  end,
+})
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,52 +1,134 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			-- install jsregexp (optional!).
-			build = "make install_jsregexp",
-		},
-		"rafamadriz/friendly-snippets",
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-	},
-	config = function()
-		local cmp = require("cmp")
-		local lspkind = require("lspkind")
-		local luasnip = require("luasnip")
+  -- Ultimate autocompletion plugin setup
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- Buffer completions
+      "hrsh7th/cmp-path", -- Path completions
+      "hrsh7th/cmp-nvim-lsp", -- LSP completions
+      "hrsh7th/cmp-nvim-lua", -- Neovim Lua API completions
+      "hrsh7th/cmp-cmdline", -- Command-line completions
+      "hrsh7th/cmp-calc", -- Calculation completions
+      "f3fora/cmp-spell", -- Spell suggestions
+      "saadparwaiz1/cmp_luasnip", -- LuaSnip source for nvim-cmp
+      "onsails/lspkind.nvim", -- VSCode-like pictograms
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+      },
+      "rafamadriz/friendly-snippets", -- Large snippet collection (includes Django)
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "calc" },
+          { name = "spell" },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            menu = {
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snip]",
+              buffer = "[Buf]",
+              path = "[Path]",
+              calc = "[Calc]",
+              spell = "[Spell]",
+            },
+          }),
+        },
+        experimental = {
+          ghost_text = true,
+        },
+      })
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			}),
-		})
+      -- Setup for command-line completion
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
 
-		vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
-	end,
+      vim.cmd([[
+        set completeopt=menu,menuone,noselect
+        highlight! default link CmpItemKind CmpItemMenuDefault
+      ]])
+    end,
+  },
+
+  -- LSP config for basedpyright (Django support)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        basedpyright = {},
+      },
+    },
+  },
+
+  -- Ensure basedpyright and other recommended tools are installed
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "basedpyright",
+        "black", -- Python formatter
+        "isort", -- Python import sorter
+        "djlint", -- Django templates linter/formatter
+      },
+    },
+  },
 }
